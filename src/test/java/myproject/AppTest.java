d598cd14fa85c3ec188174e362c7b27db11242ed
gailo22 <gailo22@gmail.com>
2011-11-16 20:22:52 +0700

    initial commit

---
+++ src/test/java/myproject/AppTest.java
@@ -0,0 +1,20 @@
+package myproject;
+
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    
+	@Test
+	public void shouldReturnTrue() {
+		assertThat(true, equalTo(true));
+		
+	}
+}
