cc4e67e471f992c0a3ab68e952ea4366230180c3
gailo22 <gailo22@gmail.com>
2011-11-21 21:27:27 +0800

    include spring

--- src/main/java/myproject/App.java
+++ src/main/java/myproject/App.java
@@ -1,17 +1,35 @@
 package myproject;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.google.common.collect.ArrayListMultimap;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-    
-    public void test2() {
-    	
-    }
+public class App {
+	public static void main(String[] args) {
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "applicationContext.xml" });
+
+		context.registerShutdownHook();
+		
+		TaskExecutorExample example = context.getBean(TaskExecutorExample.class);
+		example.printMessages();
+
+	}
+
+	public void test2() {
+		ArrayListMultimap<String, String> multiMap = ArrayListMultimap.create();
+		multiMap.put("1", "a");
+		multiMap.put("1", "aa");
+		multiMap.put("2", "b");
+		multiMap.put("3", "c");
+		multiMap.put("3", "cc");
+		multiMap.put("3", "ccc");
+		multiMap.put("4", "d");
+		System.out.println(multiMap.asMap());
+	}
 }
--- src/test/java/myproject/AppTest.java
+++ src/test/java/myproject/AppTest.java
@@ -1,19 +1,30 @@
 package myproject;
 
-
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
-
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    
+
 	@Test
 	public void shouldReturnTrue() {
+		System.out.println("testtest");
 		assertThat(true, equalTo(true));
+		System.out.println(getString());
+	}
+
+	private String getString() {
+		// TODO Auto-generated method stub
+		return "string";
+	}
+	
+	@Test
+	public void shouldPassForTest2() {
+		App app = new App();
+		app.test2();
 	}
 }
