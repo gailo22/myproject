ed6184ed9a738428101ebb0a7eec9feb46857cdf
gailo22 <gailo22@gmail.com>
2011-11-21 22:50:17 +0800

    add scheduler

---
+++ src/main/java/myproject/ExampleBusinessObject.java
@@ -0,0 +1,11 @@
+package myproject;
+
+public class ExampleBusinessObject {
+	  
+	  // properties and collaborators
+	  
+	  public void doIt() {
+	    // do the actual work
+		  System.out.println("in doIt...");
+	  }
+	}
---
+++ src/main/java/myproject/ExampleJob.java
@@ -0,0 +1,23 @@
+package myproject;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class ExampleJob extends QuartzJobBean {
+
+	  private int timeout;
+	  
+	  /**
+	   * Setter called after the ExampleJob is instantiated
+	   * with the value from the JobDetailBean (5)
+	   */ 
+	  public void setTimeout(int timeout) {
+	    this.timeout = timeout;
+	  }
+	  
+	  protected void executeInternal(JobExecutionContext ctx) throws JobExecutionException {
+	      // do the actual work
+		  System.out.println("in executeInternal.....");
+	  }
+	}
