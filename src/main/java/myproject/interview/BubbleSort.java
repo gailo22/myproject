4f293718e458595224f300731ffc5e5044d91a0c
gailo22 <gailo22@gmail.com>
2014-06-06 15:46:37 +0700

    added bubble sort, middlelinkedlist

---
+++ src/main/java/myproject/interview/BubbleSort.java
@@ -0,0 +1,37 @@
+package myproject.interview;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+
+		int[] unsorted = { 32, 39, 21, 45, 23, 3 };
+
+		System.out.println(Arrays.toString(unsorted));
+		bubbleSort(unsorted);
+		System.out.println(Arrays.toString(unsorted));
+
+	}
+
+	private static void bubbleSort(int[] unsorted) {
+		int n = unsorted.length;
+
+		for (int i = 0; i < n - 1; i++) {
+
+			for (int j = 0; j < n - i; j++) {
+				if (unsorted[j] > unsorted[j + 1]) {
+					swap(unsorted, j, j + 1);
+				}
+			}
+			
+		}
+	}
+
+	private static void swap(int[] a, int i, int j) {
+		int tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+
+}
