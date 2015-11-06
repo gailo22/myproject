f0669a991d650bd999d79d452fed9eee9a1f7ae8
unknown <montreeb@globalsources.com>
2015-11-06 13:45:25 +0800

    play with regex

---
+++ src/main/java/myproject/regex/RegexTest1.java
@@ -0,0 +1,31 @@
+package myproject.regex;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexTest1 {
+
+	public static void main(String[] args) {
+
+		String str = "The world's of mine  and they are so lazy";
+		
+		List<String> tokens = getTokens(str, "(?i)\\w+'{0,1}\\w+");
+		
+		System.out.println(tokens);
+	}
+	
+	static List<String> getTokens(String text, String pattern) {
+		ArrayList<String> tokens = new ArrayList<String>();
+		Pattern tokSplitter = Pattern.compile(pattern);
+		Matcher m = tokSplitter.matcher(text);
+		
+		while (m.find()) {
+			tokens.add(m.group());
+		}
+		
+		return tokens;
+	}
+
+}
