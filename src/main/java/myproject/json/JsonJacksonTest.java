dce4947ea338772f1c9d4685f04d12b2e702aeb0
unknown <montreeb@globalsources.com>
2015-12-08 10:06:40 +0800

    updated pom.xml

--- src/main/java/myproject/json/JsonJacksonTest.java
+++ src/main/java/myproject/json/JsonJacksonTest.java
@@ -3,20 +3,26 @@ package myproject.json;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class JsonJacksonTest {
 
 	public static void main(String[] args) throws Exception {
 		
 		Path jsonPath = Paths.get(Thread.currentThread()
-				.getContextClassLoader().getResource("hello.json").toURI());
+				                        .getContextClassLoader()
+				                        .getResource("hello.json")
+				                        .toURI());
 		
 		ObjectMapper mapper = new ObjectMapper();
 		Hello hello = mapper.readValue(jsonPath.toFile(), Hello.class);
 
 		System.out.println("jsonPath: " + jsonPath);
 		System.out.println("hello: " + hello);
+		
+		String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(hello);
+		System.out.println("jsonString: " + jsonString);
 	}
 
 }
