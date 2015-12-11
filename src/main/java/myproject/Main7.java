2dd928ccd262167cca2ad69d8400cd81947cbeb1
unknown <montreeb@globalsources.com>
2015-12-11 14:44:10 +0800

    udpated

--- src/main/java/myproject/Main7.java
+++ src/main/java/myproject/Main7.java
@@ -1,9 +1,15 @@
 package myproject;
 
 
+
 public class Main7 {
 	
 	public static void main(String[] args) {
+		
+		long start = System.currentTimeMillis();
+		
+		
+		System.out.println("Elapsed time: " + (System.currentTimeMillis() - start));
 		
 	}
 }
