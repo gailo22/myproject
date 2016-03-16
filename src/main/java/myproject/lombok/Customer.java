baaf86e534f41954abd73867243b6b118c177ed6
Montree Bungnasang  (NCS) <montreeb@ncs.com.sg>
2016-03-16 10:05:26 +0800

    play with lombok

---
+++ src/main/java/myproject/lombok/Customer.java
@@ -0,0 +1,20 @@
+package myproject.lombok;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Customer {
+	
+	private Integer id;
+    private String firstName;
+    
+    public static void main(String[] args) {
+    	CustomerBuilder builder = new CustomerBuilder();
+    	builder.id(123).firstName("Hello");
+    	
+    	System.out.println(builder.build());
+    }
+
+}
