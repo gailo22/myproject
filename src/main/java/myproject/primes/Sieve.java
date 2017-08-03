ace786a824e48872a6c6f8fb983b2d7e224039f9
gailo22 <gailo22@gmail.com>
2017-08-03 18:26:00 +0800

    play with prime number 2

--- src/main/java/myproject/primes/Sieve.java
+++ src/main/java/myproject/primes/Sieve.java
@@ -2,6 +2,7 @@ package myproject.primes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Sieve {
 
@@ -15,9 +16,21 @@ public class Sieve {
 		List<Integer> localPrimes = new ArrayList<>();
 		localPrimes.add(2);
 		for (int i = 3; i <= limit; i++) {
-			checkPrime(i, localPrimes);
+			//checkPrime(i, localPrimes);
+			checkPrime2(i, localPrimes).map(localPrimes::add);
 		}
 		return localPrimes;
+	}
+
+	private static Optional<Integer> checkPrime2(int candidate, List<Integer> primesList) {
+		int s = primesList.size();
+		for (int i = 0; i < s; i++) {
+			Integer loopPrime = primesList.get(i);
+			if (candidate % loopPrime.intValue() == 0) {
+				return Optional.empty();
+			}
+		}
+		return Optional.of(candidate);
 	}
 
 	private static void checkPrime(int candidate, List<Integer> primesList) {
